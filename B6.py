dd99ceba896333dde50fcab150e89dae2f09d7fa
AnnaDK <annadktester@gmail.com>
2021-12-01 22:51:51 +0100

    Poprawnie rozwiazane zadanie 6

---
+++ B6.py
@@ -0,0 +1,19 @@
+#Zliczać 4 do listy
+
+from random import randint
+
+how_many = int(input("Ile liczb wylosować? "))
+list = []
+for i in range(0, how_many):
+    list.append(randint(0, 100))
+print(list)
+
+added = int(input("Jaką liczbę chcesz dodać? "))
+is_4_list = []
+if added == 4:
+    list.append(added)
+    print(f'{added} może zostać dodana do listy. Aktualna lista zawiera:')
+    print (list)
+elif added != 4:
+    print (f'{added} nie może zostać dodana do listy. Aktualna lista zawiera:')
+    print (list)
