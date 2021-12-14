4a2afc8b76308062d7c23c36b581456a9e2403fa
AnnaDK <annadktester@gmail.com>
2021-12-14 10:48:15 +0100

    Równanie

---
+++ B8.py
@@ -0,0 +1,7 @@
+# Napisz program rozwiązujący równanie (x + y) * (x + y)
+print("Hej, rozwiążę za Ciebie równanie (x + y) x (x + y)")
+x = float(input("Podaj niewiadomą x: "))
+y = float(input("Podaj niewiadomą y: "))
+
+result = ((x + y) * (x + y))
+print(f'Twój wynik to {result}')
